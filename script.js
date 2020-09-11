df444575fc562382b48353a9ee35f686bf053fe0
Ebru Çiçekli <cicekliebru@gmail.com>
2020-09-11 21:05:21 +0300

    added function. Now code is a little bit more resuable

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 var form = $("#form_id");
 var movieList = $("#movieList");
+var searchButton = $(".searchButton");
 
-$(".button").click(function () {
+var getData = function () {
   var movieName = $("#movieName").val();
   console.log(movieName);
   axios
@@ -9,23 +10,34 @@ $(".button").click(function () {
     .then(function (response) {
       var res = response.data;
       console.log(res);
+      
       movieList.empty();
       $("#movieName").val("");
 
-      function listMovies(data) {
-        var html, newHtml;
-        html =
-        '<div class="column is-3"><div class="card"><div class="card-image"><figure class="image"> <img src="%poster%" /> </figure> </div> <div class="card-content"> <div class="media"><div class="media-content"> <p class="title is-4">%title%</p></div>  </div><footer class="card-footer"><p class="card-footer-item">%year</p><p class="card-footer-item">Favori icon</p></footer></div></div></div>'
-        
-        newHtml = html.replace(/%poster%/, data.Poster);
-        newHtml = newHtml.replace(/%title%/, data.Title);
-        newHtml = newHtml.replace(/%year%/, data.Year);
-        movieList.append(newHtml);
-      }
-
       res.Search.forEach(function (list) {
-        listMovies(list);
+        addItem(list);
       });
-      
     });
-});
+};
+
+var setEventHandlers = function () {
+  searchButton.click(getData);
+};
+var addItem = function (data) {
+  var html, newHtml;
+  html =
+    '<div class="column is-3"><div class="card"><div class="card-image"><figure class="image"> <img src="%poster%" /> </figure> </div> <div class="card-content"> <div class="media"><div class="media-content"> <p class="title is-4">%title%</p></div>  </div><footer class="card-footer"><p class="card-footer-item">%year%</p><p class="card-footer-item">Favori icon</p></footer></div></div></div>';
+
+  newHtml = html.replace(/%poster%/, data.Poster);
+  newHtml = newHtml.replace(/%title%/, data.Title);
+  newHtml = newHtml.replace(/%year%/, data.Year);
+  movieList.append(newHtml);
+};
+
+var appController = {
+  init: function () {
+    return setEventHandlers();
+  },
+};
+
+appController.init();
