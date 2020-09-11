30f38c2ff9054dac250939a03a3f0dcba9f64d39
Ebru Çiçekli <cicekliebru@gmail.com>
2020-09-11 20:41:48 +0300

    change to axios for get data and added card view

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-var movieName = $("#movieName").val();
 var form = $("#form_id");
 var movieList = $("#movieList");
 
-form.on("submit", function (event) {
-  event.preventDefault();
-  $.ajax({
-    method: "GET",
-    url: "http://www.omdbapi.com/?s=" + movieName + "&apikey=b9af8b2e",
-  }).then(function (response) {
-    console.log(response);
-    movieList.html("");
-    response.Search.forEach(function (list) {
-      movieList.append(
-        "<li>" +
-          "<h2>" +
-          list.Title +
-          "</h2>" +
-          "<p>" +
-          list.Year +
-          "</p>" +
-          "<img src='" +
-          list.Poster +
-          "'>" +
-          "</li>"
-      );
+$(".button").click(function () {
+  var movieName = $("#movieName").val();
+  console.log(movieName);
+  axios
+    .get("http://www.omdbapi.com/?s=" + movieName + "&apikey=b9af8b2e")
+    .then(function (response) {
+      var res = response.data;
+      console.log(res);
+      movieList.empty();
+      $("#movieName").val("");
+
+      function listMovies(data) {
+        var html, newHtml;
+        html =
+        '<div class="column is-3"><div class="card"><div class="card-image"><figure class="image"> <img src="%poster%" /> </figure> </div> <div class="card-content"> <div class="media"><div class="media-content"> <p class="title is-4">%title%</p></div>  </div><footer class="card-footer"><p class="card-footer-item">%year</p><p class="card-footer-item">Favori icon</p></footer></div></div></div>'
+        
+        newHtml = html.replace(/%poster%/, data.Poster);
+        newHtml = newHtml.replace(/%title%/, data.Title);
+        newHtml = newHtml.replace(/%year%/, data.Year);
+        movieList.append(newHtml);
+      }
+
+      res.Search.forEach(function (list) {
+        listMovies(list);
+      });
+      
     });
-  });
 });
