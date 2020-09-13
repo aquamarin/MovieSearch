a2dadbd1e03fc10ae3a93fb7f14bd8ad535bf1a0
Ebru Çiçekli <cicekliebru@gmail.com>
2020-09-13 23:42:02 +0300

    a couple problem solved and edit localStorage function for favorites

--- script.js
+++ script.js
@@ -4,13 +4,24 @@ var searchButton = $(".searchButton");
 var itemName = $("#inputName");
 var searchName = $(".searchBox");
 var searchItems = [];
+var favoritesList = [];
+var keyForSearch = "searchWords";
+var keyForFavorites = "favorites";
+var favoriteName = $("#favoritesList");
+
+var uuidv4 = function () {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+};
 
 var getData = function (data) {
   searchData = data ? data : itemName.val();
   axios
     .get("http://www.omdbapi.com/?s=" + searchData + "&apikey=b9af8b2e")
     .then(function (response) {
-      console.log(searchData);
       var res = response.data;
 
       FuncForLocalStorage.search(searchData);
@@ -18,41 +29,67 @@ var getData = function (data) {
       itemName.val("");
 
       res.Search.forEach(function (list) {
-        addItem(list);
+        addItem(movieList, list);
       });
       // addItem ile iconun olduğu yer eklendiği için event burada çağrılmalıdır.
-      $(".icon").click(function () {
-        var isFarOrFas = $(this).find("svg");
-        if (isFarOrFas.attr("data-prefix") == "far") {
-          isFarOrFas.attr("data-prefix", "fas");
-        } else {
-          isFarOrFas.attr("data-prefix", "far");
-        }
-      });
+      setRecursiveFunc();
     });
 };
 var controlSearchInput = function () {
-  var character = itemName.val().length;
-  if (character >= 3) {
+  if (itemName.val().length >= 3) {
     itemName.removeClass("is-danger");
-    getData()
+    return true;
   } else {
-    itemName.toggleClass("is-danger");
-    
+    itemName.addClass("is-danger");
+    return false;
   }
 };
 var setEventHandlers = function () {
-  searchButton.click(controlSearchInput);
+  var flag = false;
 
   $(document).keypress(function (event) {
+    flag = controlSearchInput();
     if (event.keyCode == "13" || event.which == "13") {
       event.preventDefault();
-      controlSearchInput();
+      if (flag) {
+        getData();
+      }
+    }
+  });
+  searchButton.click(function () {
+    if (flag) {
+      getData();
+    }
+  });
+
+  setRecursiveFunc();
+};
+var setRecursiveFunc = function () {
+  $(".icon").click(function () {
+    var isFarOrFas = $(this).find("svg");
+    var Etarget;
+    if (isFarOrFas.attr("data-prefix") == "far") {
+      isFarOrFas.attr("data-prefix", "fas");
+      Etarget = $(this).parents(".column");
+      var id = uuidv4();
+      var favorite = {
+        id: id,
+        div: Etarget,
+      };
+      favoritesList.push(favorite);
+      Etarget.attr("id", id);
+      Etarget.clone().appendTo(favoriteName);
+      FuncForLocalStorage.setItem(keyForFavorites, favoritesList);
+      //addItem(favoriteName);
+    } else {
+      isFarOrFas.attr("data-prefix", "far");
+      Etarget = $(this).parents(".column");
+      FuncForLocalStorage.deleteFavoritesItem(Etarget.attr("id"));
     }
   });
   $(".closeIcon").click(function () {
     var delItem = $(this).parent().parent().attr("key");
-    FuncForLocalStorage.deleteItem(delItem);
+    FuncForLocalStorage.deleteItem(keyForSearch, delItem);
     displaySearchItems(searchItems);
   });
   $(".box").click(function () {
@@ -60,7 +97,7 @@ var setEventHandlers = function () {
     getData(movieData);
   });
 };
-var addItem = function (data) {
+var addItem = function (list, data) {
   var html, newHtml;
   html = `<div class="column is-3">
     <div class="card">
@@ -95,7 +132,7 @@ var addItem = function (data) {
   }
   newHtml = newHtml.replace(/%title%/, data.Title);
   newHtml = newHtml.replace(/%year%/, data.Year);
-  movieList.append(newHtml);
+  list.append(newHtml);
 };
 
 var displaySearchItems = function (searchItems) {
@@ -121,6 +158,7 @@ var displaySearchItems = function (searchItems) {
         </div>`;
     searchName.prepend(box);
   });
+  setRecursiveFunc();
 };
 
 var FuncForLocalStorage = (function () {
@@ -131,26 +169,16 @@ var FuncForLocalStorage = (function () {
         searchItems.splice($.inArray(searchItems[i], searchItems), 1);
       }
     }
-    deleteLocalStorage();
-    setItemLocalStorage(searchItems);
+    deleteLocalStorage(keyForSearch);
+    setItemLocalStorage(keyForSearch, searchItems);
     displaySearchItems(searchItems);
   };
 
-  var setItemLocalStorage = function (obj) {
-    localStorage.setItem("searchWords", JSON.stringify(obj));
+  var setItemLocalStorage = function (key, obj) {
+    localStorage.setItem(key, JSON.stringify(obj));
   };
-  var deleteLocalStorage = function () {
-    localStorage.removeItem("searchWords");
-  };
-
-  var uuidv4 = function () {
-    return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (
-      c
-    ) {
-      var r = (Math.random() * 16) | 0,
-        v = c == "x" ? r : (r & 0x3) | 0x8;
-      return v.toString(16);
-    });
+  var deleteLocalStorage = function (key) {
+    localStorage.removeItem(key);
   };
   return {
     // aranan kelimeyi setItems arrayıne obje olarak ekliyor
@@ -160,18 +188,34 @@ var FuncForLocalStorage = (function () {
         name: word,
       };
       searchItems.push(search);
-      setItemLocalStorage(searchItems);
+      setItemLocalStorage(keyForSearch, searchItems);
       if (searchItems.length > 10) {
         deleteSearchItem(searchItems[0].id);
       }
       displaySearchItems(searchItems);
     },
 
-    getLocalStorage: function () {
-      return JSON.parse(localStorage.getItem("searchWords"));
+    getLocalStorage: function (key) {
+      return JSON.parse(localStorage.getItem(key));
     },
-    deleteItem: function (id) {
-      deleteSearchItem(id);
+    deleteItem: function (key, id) {
+      if (key == keyForSearch) {
+        deleteSearchItem(id);
+      } else {
+        deleteFavoritesItem(id);
+      }
+    },
+    setItem: function (key, obj) {
+      setItemLocalStorage(key, obj);
+    },
+    deleteFavoritesItem: function (id) {
+      for (var i = 0; i < favoritesList.length; i++) {
+        if (favoritesList[i].id == id) {
+          favoritesList.splice($.inArray(favoritesList[i], favoritesList), 1);
+        }
+      }
+      deleteLocalStorage(keyForFavorites);
+      setItemLocalStorage(keyForFavorites, favoritesList);
     },
   };
 })();
@@ -179,8 +223,8 @@ var FuncForLocalStorage = (function () {
 var appController = {
   init: function () {
     var FFLStorage = FuncForLocalStorage;
-    if (FFLStorage.getLocalStorage() !== null) {
-      searchItems = FFLStorage.getLocalStorage();
+    if (FFLStorage.getLocalStorage(keyForSearch) !== null) {
+      searchItems = FFLStorage.getLocalStorage(keyForSearch);
       displaySearchItems(searchItems);
     }
     return setEventHandlers();
