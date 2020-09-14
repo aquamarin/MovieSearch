271a51e9228b87fbae710841f24cd3149127a998
Ebru Çiçekli <cicekliebru@gmail.com>
2020-09-14 17:33:24 +0300

    I am trying to move with pattern but first i m orginazed to project

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var DOMStrings = {
   movieList: $("#movieList"),
   searchButton: $(".searchButton"),
   itemName: $("#inputName"),
-  searchName: $(".searchBox"),
+  searchList: $(".searchBox"),
   keyForSearch: "searchWords",
   keyForFavorites: "favorites",
   favoriteName: $("#favoritesList"),
@@ -94,8 +94,7 @@ var setRecursiveFunc = function () {
         div: Etarget,
       };
       favoritesList.push(favorite);
-      Etarget.attr("id", id);
-      Etarget.clone().appendTo(DOMStrings.favoriteName);
+      displayFavoriteItems(favoritesList);
       FuncForLocalStorage.setItem(DOMStrings.keyForFavorites, favoritesList);
       //addItem(favoriteName);
     } else {
@@ -103,19 +102,25 @@ var setRecursiveFunc = function () {
 
       //favori listesinden elemanı çıkarmak için
       Etarget = $(this).parents(".column");
-      FuncForLocalStorage.deleteFavoritesItem(Etarget.attr("id"));
+      FuncForLocalStorage.deleteItem(
+        DOMStrings.keyForFavorites,
+        Etarget.attr("id"),
+        favoritesList
+      );
     }
   });
 
   // önceden aratılmış olan ögeler listesinden eleman çıkarmak için.
   $(".closeIcon").click(function () {
-    var delItem = $(this).parent().parent().attr("key");
-    FuncForLocalStorage.deleteItem(DOMStrings.keyForSearch, delItem);
+    var delItem = $(this).closest(".searchItem").attr("key");
+    DOMStrings.searchList = DOMStrings.searchList.filter(
+      (obj) => obj.id !== delItem
+    );
     displaySearchItems(searchItems);
   });
 
-  //önceden aratılmış olan ögeler listesindeki herhangi bir ögeye 
-  //tıklandığında tekrar aratılması için 
+  //önceden aratılmış olan ögeler listesindeki herhangi bir ögeye
+  //tıklandığında tekrar aratılması için
   $(".box").click(function () {
     var movieData = $(this).find("p").text().trim();
     getData(movieData);
@@ -164,95 +169,63 @@ var addItem = function (list, data) {
 // Search edilen ögelerin görüntülenmesi için.
 var displaySearchItems = function (searchItems) {
   //aynı ögelerin tekrar etmemesi için display etmeden önce temizliyoruz.
-  DOMStrings.searchName.children().remove();
+  DOMStrings.searchList.children().remove();
   searchItems.forEach(function (item) {
-    var itemID, itemName, box;
-    itemID = item.id;
-    itemName = item.name;
-
-    box =
-      `<div key="` +
-      itemID +
-      `" class="box column is-one-quarter">
-            <div class="tile">
-            <p> ` +
-      itemName +
-      `</p>
+    var html = `<div key="%key%" class="searchItem box column is-one-quarter">
+          <div class="tile">
+            <p>%name%</p>
             <span class="closeIcon">
               <i class="fas fa-times fa-2x"></i>
             </span>
           </div>
         </div>`;
-    DOMStrings.searchName.prepend(box);
+    html = html.replace(/%key%/, item.id);
+    html = html.replace(/%name%/, item.name);
+    DOMStrings.searchList.prepend(html);
+  });
+  setRecursiveFunc();
+};
+displayFavoriteItems = function (favoriteItems) {
+  DOMStrings.favoriteName.children().remove();
+  favoriteItems.forEach(function (item) {
+    var itemID, itemDiv;
+    itemID = item.id;
+    itemDiv = item.div;
+    itemDiv.attr("id", itemID);
+    itemDiv.clone().appendTo(DOMStrings.favoriteName);
+    //DOMStrings.favoriteName.prepend(itemDiv);
   });
   setRecursiveFunc();
 };
 
 //localstorage olaylarının döndüğü yer
-var FuncForLocalStorage = (function () {
-  //id yi alıp aynı idye sahip olan objeyi siliyor
-  var deleteSearchItem = function (id) {
-    for (var i = 0; i < searchItems.length; i++) {
-      if (searchItems[i].id == id) {
-        searchItems.splice($.inArray(searchItems[i], searchItems), 1);
-      }
+var storage = {
+  // aranan kelimeyi setItems arrayıne obje olarak ekliyor
+  search: function (word) {
+    var search = {
+      id: uuidv4(),
+      name: word,
+    };
+    searchItems.push(search);
+    setItem(DOMStrings.keyForSearch, searchItems);
+    if (searchItems.length > 10) {
+      DOMStrings.searchList.shift();
     }
-    deleteLocalStorage(DOMStrings.keyForSearch);
-    setItemLocalStorage(DOMStrings.keyForSearch, searchItems);
     displaySearchItems(searchItems);
-  };
+  },
 
-  var setItemLocalStorage = function (key, obj) {
+  getLocalStorage: function (key) {
+    return JSON.parse(localStorage.getItem(key));
+  },
+  setItem: function (key, obj) {
     localStorage.setItem(key, JSON.stringify(obj));
-  };
-  var deleteLocalStorage = function (key) {
-    localStorage.removeItem(key);
-  };
-  return {
-    // aranan kelimeyi setItems arrayıne obje olarak ekliyor
-    search: function (word) {
-      var search = {
-        id: uuidv4(),
-        name: word,
-      };
-      searchItems.push(search);
-      setItemLocalStorage(DOMStrings.keyForSearch, searchItems);
-      if (searchItems.length > 10) {
-        deleteSearchItem(searchItems[0].id);
-      }
-      displaySearchItems(searchItems);
-    },
-
-    getLocalStorage: function (key) {
-      return JSON.parse(localStorage.getItem(key));
-    },
-    deleteItem: function (key, id) {
-      if (key == DOMStrings.keyForSearch) {
-        deleteSearchItem(id);
-      } else {
-        deleteFavoritesItem(id);
-      }
-    },
-    setItem: function (key, obj) {
-      setItemLocalStorage(key, obj);
-    },
-    deleteFavoritesItem: function (id) {
-      for (var i = 0; i < favoritesList.length; i++) {
-        if (favoritesList[i].id == id) {
-          favoritesList.splice($.inArray(favoritesList[i], favoritesList), 1);
-        }
-      }
-      deleteLocalStorage(DOMStrings.keyForFavorites);
-      setItemLocalStorage(DOMStrings.keyForFavorites, favoritesList);
-    },
-  };
-})();
+  },
+};
 
 var appController = {
   init: function () {
-    var FFLStorage = FuncForLocalStorage;
-    if (FFLStorage.getLocalStorage(DOMStrings.keyForSearch) !== null) {
-      searchItems = FFLStorage.getLocalStorage(DOMStrings.keyForSearch);
+    if (storage.getLocalStorage(DOMStrings.keyForSearch) !== null) {
+      searchItems = storage.getLocalStorage(DOMStrings.keyForSearch);
       displaySearchItems(searchItems);
     }
     return setEventHandlers();
